0839d2d386c9a0ceaf30c8550614276f5bd86b35
Abdul Rehman Ansari <iabdul2002@gmail.com>
2026-02-12 05:01:39 +0530

    08-tax-calculator

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,21 @@
  */
 export function calculateTax(income) {
   // Your code here
+  if(income <= 0){
+    return 0;
+  }
+  let tax = 0 ;
+  if(income <= 10000){
+    return tax;
+  }
+  else if( income <= 30000 ){
+    tax = (income - 10000) * 0.1 ;
+  }
+  else if( income <= 70000){
+    tax = (income - 30000) * 0.2 + 2000 ;
+  }
+  else{
+    tax = (income - 70000) * 0.3 + 8000 + 2000 ;
+  }
+  return tax;
 }
